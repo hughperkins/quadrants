6cdd0e3a82f948f57f9b38562f02e7dfa025cabd
Hugh Perkins <hughperkins@gmail.com>
2025-04-28 20:45:52 -0400

    snode_registroy

--- taichi/python/snode_registry.cpp
+++ taichi/python/snode_registry.cpp
@@ -7,6 +7,7 @@
 namespace taichi::lang {
 
 SNode *SNodeRegistry::create_root(Program *prog) {
+  std::cout << "SNodeRegistry create root" << std::endl;
   TI_ASSERT(prog != nullptr);
   auto n = std::make_unique<SNode>(/*depth=*/0, SNodeType::root,
                                    prog->get_snode_to_fields(),
@@ -17,12 +18,18 @@ SNode *SNodeRegistry::create_root(Program *prog) {
 }
 
 std::unique_ptr<SNode> SNodeRegistry::finalize(const SNode *snode) {
+  std::cout << "Finalizing SNode: " << snode->get_name() << std::endl;
+  auto i = 0;
   for (auto it = snodes_.begin(); it != snodes_.end(); ++it) {
+    std::cout << "SNodeRegistry snodes_[" << i << "] = " << (*it)->get_name()
+              << std::endl;
     if (it->get() == snode) {
+      std::cout << "SNodeRegistry found the node" << std::endl;
       auto res = std::move(*it);
       snodes_.erase(it);
       return res;
     }
+    i += 1;
   }
   return nullptr;
 }
