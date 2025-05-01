30edf6b88327062526f7e5cd10b8ef6f1681fd0f
Hugh Perkins <hughperkins@gmail.com>
2025-04-30 20:31:37 -0400

    comment otu snode tree

--- python/taichi/lang/impl.py
+++ python/taichi/lang/impl.py
@@ -188,7 +188,7 @@ def validate_subscript_index(value, index):
 def subscript(ast_builder, value, *_indices, skip_reordered=False):
     print("lang.impl.py subscript", value)
     # print("dir(value.snode)", dir(value.snode.ptr))
-    print("   snode tree id", value.snode.ptr.get_snode_tree_id())
+    # print("   snode tree id", value.snode.ptr.get_snode_tree_id())
     # , value.snode.ptr.get_snode_tree_id())
     dbg_info = _ti_core.DebugInfo(get_runtime().get_current_src_info())
     ast_builder = get_runtime().compiling_callable.ast_builder()
