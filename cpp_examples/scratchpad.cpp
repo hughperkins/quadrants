3de055dd21f049f035c0f6685125a1e37b50c629
Hugh Perkins <hughperkins@gmail.com>
2025-05-03 19:58:06 -0400

    trying to bypass optimization, but not sure if succeeded

--- cpp_examples/scratchpad.cpp
+++ cpp_examples/scratchpad.cpp
@@ -3,6 +3,10 @@
 #include "taichi/ir/ir_builder.h"
 #include "taichi/ir/statements.h"
 #include "taichi/program/program.h"
+#include "taichi/runtime/program_impls/llvm/llvm_program.h"
+#include "taichi/codegen/llvm/kernel_compiler.h"
+// #include "taichi/codegen/codegen.h"
+#include "taichi/ir/analysis.h"
 
 using namespace taichi;
 using namespace lang;
@@ -22,6 +26,60 @@ void writeResult(Block *block, int idx, Stmt *value) {
   auto globalStore0 =
       std::unique_ptr<GlobalStoreStmt>(new GlobalStoreStmt(arg0Ptr, value));
   block->insert(std::move(globalStore0));
+}
+
+// std::unique_ptr<IRNode> minimal_prepare_ir(const Kernel &kernel) {
+//     auto ir = irpass::analysis::clone(kernel.ir.get());
+
+//     // Do minimal transformations required for code generation
+//     irpass::type_check(ir.get(), kernel.get_program().compile_config());
+//     irpass::offload(ir.get(), kernel.get_program().compile_config());
+//     irpass::flag_access(ir.get());
+
+//     return ir;
+// }
+
+std::unique_ptr<IRNode> minimal_prepare_ir(const Kernel &kernel,
+                                           const CompileConfig &config) {
+  auto ir = irpass::analysis::clone(kernel.ir.get());
+
+  // For compatibility with what KernelCompiler expects, we need some basic
+  // transforms But only the minimal required ones
+
+  // First, let's create an offloaded block
+  auto root = ir.get();
+  if (root->is<Block>()) {
+    auto block = root->as<Block>();
+    // Create an OffloadedStmt that contains the original block
+    auto offloaded = std::make_unique<OffloadedStmt>(
+        OffloadedStmt::TaskType::serial, config.arch,
+        const_cast<Kernel *>(
+            &kernel)  // Need to cast away const since OffloadedStmt doesn't
+                      // accept const Kernel*
+    );
+    offloaded->body = std::make_unique<Block>();
+
+    // Move all statements from the root block to the offloaded block body
+    for (int i = 0; i < block->size(); i++) {
+      offloaded->body->insert(std::move(block->statements[i]));
+    }
+    block->statements.clear();
+
+    // Add the offloaded task to the root block
+    block->insert(std::move(offloaded));
+  }
+
+  // Set types for statements that need it
+  irpass::analysis::gather_statements(ir.get(), [](Stmt *stmt) {
+    if (stmt->ret_type == DataType()) {
+      if (stmt->is<ConstStmt>()) {
+        stmt->ret_type = stmt->as<ConstStmt>()->val.dt;
+      }
+    }
+    return false;
+  });
+
+  return ir;
 }
 
 void writeIR(Block *block) {
@@ -79,8 +137,23 @@ int main() {
   kernel_ret->finalize_params();
 
   auto ctx_ret = kernel_ret->make_launch_context();
-  const auto &compiled_kernel_data =
-      program.compile_kernel(config, program.get_device_caps(), *kernel_ret);
+
+  auto program_impl = program.get_program_impl();
+  auto llvm_program = static_cast<LlvmProgramImpl *>(program_impl);
+  auto tlctx = llvm_program->get_llvm_context();
+
+  LLVM::KernelCompiler::Config compiler_config;
+  compiler_config.tlctx = tlctx;
+  LLVM::KernelCompiler kernel_compiler(compiler_config);
+
+  auto prepared_ir = minimal_prepare_ir(*kernel_ret, program.compile_config());
+
+  auto compiled_kernel_data = kernel_compiler.compile(
+      config, program.get_device_caps(), *kernel_ret, *prepared_ir);
+
+  //   const auto &compiled_kernel_data =
+  //       program.compile_kernel(config, program.get_device_caps(),
+  //       *kernel_ret);
   program.materialize_runtime();
 
   const int size = 20;
@@ -89,7 +162,7 @@ int main() {
   ctx_ret.set_arg_external_array_with_shape(
       /*arg_id=*/{0}, (uint64)array.get(), size, {size});
 
-  program.launch_kernel(compiled_kernel_data, ctx_ret);
+  program.launch_kernel(*compiled_kernel_data, ctx_ret);
   for (int i = 0; i < size; i++) {
     std::cout << "array[" << i << "] = " << array[i] << std::endl;
   }
