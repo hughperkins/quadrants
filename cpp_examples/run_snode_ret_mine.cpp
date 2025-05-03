302abe62a7590eda16406a49d92a2c7aad80f693
Hugh Perkins <hughperkins@gmail.com>
2025-05-03 16:02:38 -0400

    simplest possible example

--- cpp_examples/run_snode_ret_mine.cpp
+++ cpp_examples/run_snode_ret_mine.cpp
@@ -4,18 +4,6 @@
 #include "taichi/ir/statements.h"
 #include "taichi/program/program.h"
 
-std::unique_ptr<taichi::lang::IRNode> get_ir_unique_ptr(
-    taichi::lang::Block *block) {
-  auto new_block = std::make_unique<taichi::lang::Block>();
-  for (auto &stmt : block->statements) {
-    auto stmt_clone = stmt->clone();
-    new_block->insert(std::move(stmt_clone));
-  }
-
-  // Return the new block as an IRNode
-  return new_block;
-}
-
 int main() {
   using namespace taichi;
   using namespace lang;
@@ -32,26 +20,13 @@ int main() {
   auto const_type = PrimitiveType::i32;
   auto const_123 =
       block->push_back<ConstStmt>(TypedConstant(PrimitiveType::i32, 123));
-  auto ret_stmt =
-      static_cast<ReturnStmt *>(block->push_back<ReturnStmt>(const_123));
-  ret_stmt->ret_type = PrimitiveType::i32;
-  std::cout << "ret stmt " << ret_stmt->element_types().size() << std::endl;
-  std::cout << "ret stmt " << ret_stmt->element_types().begin()->to_string()
-            << std::endl;
-  // std::cout << "ret stmt " <<
-  // ret_stmt->ret_type->get_flattened_num_elements() << std::endl;
+  block->push_back<ReturnStmt>(const_123);
 
-  std::cout << "ret values " << ret_stmt->values.size() << std::endl;
-  std::cout << "ret values " << ret_stmt->values.size() << " "
-            << ret_stmt->values[0]->name() << std::endl;
   kernel_ret = std::make_unique<Kernel>(program, block.release(), "ret");
-  // kernel_ret->ret_type = PrimitiveType::i32;
   kernel_ret->insert_ret(PrimitiveType::i32);
   kernel_ret->finalize_rets();
-  std::cout << "kernel ret " << kernel_ret->ret_type << std::endl;
 
   auto ctx_ret = kernel_ret->make_launch_context();
-  std::cout << "ctx result buffer " << ctx_ret.result_buffer_size << std::endl;
   const auto &compiled_kernel_data =
       program.compile_kernel(config, program.get_device_caps(), *kernel_ret);
   program.materialize_runtime();
