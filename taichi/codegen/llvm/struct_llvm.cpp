fedf0cc0d7bac29d8bb98572bff862637571e006
Hugh Perkins <hughperkins@gmail.com>
2025-06-11 13:41:47 -0700

    shortcut refine coordiantes

--- taichi/codegen/llvm/struct_llvm.cpp
+++ taichi/codegen/llvm/struct_llvm.cpp
@@ -171,18 +171,27 @@ void StructCompilerLLVM::generate_refine_coordinates(SNode *snode) {
   for (int i = 0; i < taichi_max_num_indices; i++) {
     auto addition = tlctx_->get_constant(0);
     if (snode->extractors[i].shape > 1) {
+      std::cout << "Refining coordinates for " << snode->node_type_name
+                << " at index " << i << std::endl;
+      auto extr = snode->extractors[i];
+      std::cout << "  extr acc_shape: " << extr.acc_shape
+                << ", shape: " << extr.shape << std::endl;
       auto prev = tlctx_->get_constant(snode->extractors[i].acc_shape *
                                        snode->extractors[i].shape);
       auto next = tlctx_->get_constant(snode->extractors[i].acc_shape);
       // Use UDiv/URem instead of SDiv/SRem so that LLVM can optimize them
       // into bitwise operations when the divisor is a power of two.
+      std::cout << "   urem " << (snode->extractors[i].acc_shape *
+                                       snode->extractors[i].shape) << " udiv " << (snode->extractors[i].acc_shape) << std::endl;
       addition = builder.CreateUDiv(builder.CreateURem(l, prev), next);
     }
     auto in = call(&builder, "PhysicalCoordinates_get_val", inp_coords,
                    tlctx_->get_constant(i));
-    in =
-        builder.CreateMul(in, tlctx_->get_constant(snode->extractors[i].shape));
-    auto added = builder.CreateAdd(in, addition);
+    //   std::cout << "   mul " << snode->extractors[i].shape << std::endl;
+    // in =
+    //     builder.CreateMul(in, tlctx_->get_constant(snode->extractors[i].shape));
+    // auto added = builder.CreateAdd(in, addition);
+    auto added = in;
     call(&builder, "PhysicalCoordinates_set_val", outp_coords,
          tlctx_->get_constant(i), added);
   }
