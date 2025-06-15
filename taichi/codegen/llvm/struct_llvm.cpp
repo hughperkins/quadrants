250c1a2b26f33598cfbd0ed1a5200c4d14266ab1
Hugh Perkins <hughperkins@gmail.com>
2025-06-14 21:11:44 -0700

    add in recording and output of offsets

--- taichi/codegen/llvm/struct_llvm.cpp
+++ taichi/codegen/llvm/struct_llvm.cpp
@@ -59,14 +59,27 @@ void StructCompilerLLVM::generate_types(SNode &snode) {
       // Bit-level SNodes do not really have a corresponding LLVM type
       auto ch = get_llvm_node_type(module.get(), snode.ch[i].get());
       if(ch->isArrayTy()) {
-      std::cout << " ch array num elements " <<
-         " array num elements " << ch->getArrayNumElements() << 
-         " is array ty " << ch->isArrayTy() << " " <<  " is vector ty " << ch->isVectorTy() << std::endl;
+        std::cout << " ch array num elements " <<
+          " array num elements " << ch->getArrayNumElements() << 
+          " is array ty " << ch->isArrayTy() << " " <<  " is vector ty " << ch->isVectorTy() << std::endl;
+        auto elem_ty = llvm::cast<llvm::ArrayType>(ch)->getElementType();
+        std::cout << " array elem type: ";
+        elem_ty->print(llvm::outs());
+        llvm::outs() << "\n";
+        // std::cout << "elem_ty integer bit width " << elem_ty->getIntegerBitWidth() << " bits\n";
+        // std::cout << elem_ty->
+        const llvm::DataLayout &dl = module->getDataLayout();
+        std::cout << "elem_ty size in bytes: " << dl.getTypeAllocSize(elem_ty) << "\n";
+        
+        curr_offset += dl.getTypeAllocSize(elem_ty) * ch->getArrayNumElements();
       }
       ch->print(llvm::outs());
       llvm::outs() << "\n";
       ch_types.push_back(ch);
     }
+  }
+  for(auto i = 0; i < ch_offsets.size(); i++) {
+    std::cout << " ch " << i << " " << ch_snode_ids[i] << " offset: " << ch_offsets[i] << std::endl;
   }
   std::cout << "StructCompilerLLVM::generate_types: "
             << "ch_types.size() = " << ch_types.size() << "\n";
